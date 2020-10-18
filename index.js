46e24e730cd90b0e13388c86f217998a5d70ea90
daniel pitimson <pt21071996@gmail.com>
2020-10-18 05:48:24 +0300

    step1

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 console.log("usha");
 
+var size = 4;
 
-var para = document.createElement("p");
-var node = document.createTextNode("This is new.");
-para.appendChild(node);
+var ans = [
+    [0, 1, 1, 1],
+    [1, 1, 0, 0],
+    [0, 1, 0, 1],
+    [1, 1, 0, 0]
+];
 
-var element = document.getElementById("div1");
-element.appendChild(para);
-var x= 3;
-x = 7;
-x =19;
+var table = document.getElementById('game');
+
+var clicked = 1;
+
+for (let i = 0; i < size; i++) {
+    let row = document.createElement('tr');
+    for(let j = 0; j < size; j++){
+        let box = document.createElement('td');
+        box.style.backgroundColor = "rgb(255, 255, 255)";
+        box.id = i*4+j;
+        table.appendChild(row);
+        row.appendChild(box);
+        box.addEventListener('mousedown', (e) => {
+            e = e || window.event;
+            e = e.target;
+            if(clicked){
+                if(getComputedStyle(box).backgroundColor == "rgb(255, 255, 255)") {
+                    console.log(parseInt(e.id/size), e.id%size);
+                    if(ans[parseInt(e.id/size)][e.id%size] == 1)     
+                        e.style.backgroundColor = "#000000";
+                    else
+                        e.style.backgroundColor = "#ffcccc";
+                }
+            }
+        });
+
+    }
+}
